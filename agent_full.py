af2670a66c3670e2e62579e4b6a2b4cd71089192
Andrea Censi <acensi@ethz.ch>
2020-11-13 16:59:23 +0100

    minor

--- agent_full.py
+++ agent_full.py
@@ -2,7 +2,6 @@
 from aido_agents import FullAgent
 from aido_schemas import protocol_agent_DB20_fullstate, wrap_direct
 
-
 def main() -> None:
     node = FullAgent()
     protocol = protocol_agent_DB20_fullstate
