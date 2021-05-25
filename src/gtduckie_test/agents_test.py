344fde18f34cf3d0bb54da886dbac523dbb67c1b
alezana <azanardi@ethz.ch>
2021-05-25 14:20:30 +0200

    minors

--- src/gtduckie_test/agents_test.py
+++ src/gtduckie_test/agents_test.py
@@ -1,21 +1,23 @@
-from math import sin, cos
-from typing import Tuple
-
+from math import sin, cos, pi
+from typing import Tuple, Union
 import contracts
-import matplotlib.pyplot as plt
-from duckietown_world.world_duckietown.sampling import sample_many_good_starting_poses
+from geometry import SE2value, SE2_from_translation_angle
+
+from gtduckie.utils import euclidean_between_SE2value
 
 contracts.disable_all()
+import matplotlib.pyplot as plt
+from duckietown_world.world_duckietown.sampling import sample_many_good_starting_poses
 import numpy as np
+import duckietown_world as dw
 from duckietown_world import get_lane_poses, SE2Transform, relative_pose
 
 from gtduckie import MyFullAgent
 
-contracts.disable_all()
-import duckietown_world as dw
 
-
-def arrow_from_SE2Transform(p: SE2Transform) -> Tuple[Tuple[float, float], Tuple[float, float]]:
+def arrow_from_SE2(p: Union[SE2Transform, SE2value]) -> Tuple[Tuple[float, float], Tuple[float, float]]:
+    if isinstance(p, SE2value):
+        p = SE2Transform.from_SE2(p)
     x, y = p.p
     l = .1
     dx = cos(p.theta) * l
@@ -53,14 +55,14 @@ def test_agent():
         agent.mypose = q
         print("Lane segment control points: ", agent.pure_pursuit.path.control_points)
         for ctr_point in agent.pure_pursuit.path.control_points:
-            tail, head = arrow_from_SE2Transform(ctr_point)
+            tail, head = arrow_from_SE2(ctr_point)
             ax.arrow(tail[0], tail[1], head[0], head[1], head_width=0.01, head_length=0.05, fc='k', ec='k')
 
         print("Agent mypose: ", SE2Transform.from_SE2(agent.mypose))
 
         rel = relative_pose(agent.myglpr.lane_segment_transform.asmatrix2d().m, q)
         print("Agent relative pose [rel]: ", SE2Transform.from_SE2(rel))
-        tail, head = arrow_from_SE2Transform(SE2Transform.from_SE2(rel))
+        tail, head = arrow_from_SE2(rel)
         ax.arrow(tail[0], tail[1], head[0], head[1], head_width=0.01, head_length=0.05, fc='r', ec='r')
 
         agent.pure_pursuit.update_pose(rel, agent.myglpr.lane_pose.along_lane)
@@ -78,3 +80,21 @@ def test_agent():
         plt.show()
 
         # ipython_draw_html(m);
+
+
+def test_speed_behavior():
+    ax = plt.axes()
+    p1 = SE2_from_translation_angle([0, 0], 0)
+    p2 = SE2_from_translation_angle([1, -1],  pi / 4)
+    p3 = SE2_from_translation_angle([0.3, .2], - pi)
+    for p in [p1, p2, p3]:
+        tail, head = arrow_from_SE2(p)
+        ax.arrow(tail[0], tail[1], head[0], head[1], head_width=0.03, head_length=0.06, fc='r', ec='r')
+
+    print("Distance: ", euclidean_between_SE2value(p1, p2))
+    print("Rel pose: ", SE2Transform.from_SE2(relative_pose(p1, p2)))
+    print("Rel pose: ", SE2Transform.from_SE2(relative_pose(p1, p3)))
+
+    plt.xlim(-1.5, 1.5)
+    plt.ylim(-1.5, 1.5)
+    plt.show()
